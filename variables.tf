8c6c7af24c357cb4a5128a62eac2892d47449d09
Basil <146659417+BasilTAlias@users.noreply.github.com>
2025-02-05 17:45:42 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,11 +8,6 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "vpc_name" {
-  type    = string
-  default = "my-terraform-vpc"
-}
-
 variable "public_subnet_cidr" {
   type    = string
   default = "10.0.1.0/24"
